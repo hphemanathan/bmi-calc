bd4149a681558c4d78d04c91c9bc9106bd7c41c3
Hemanathan H P <hphemanathan@gmail.com>
2024-09-24 01:44:24 +0530

    wiplp

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('checking');
+const metricButton = document.getElementById('Metric');
+const ImperialButton = document.getElementById("Imperial");
+const ImperialInput = document.querySelector(".section__calc-form--imperial")
+
+console.log (ImperialButton)
+
+function inputCheck () {
+  if (ImperialButton.checked) {
+    console.log("working");
+    ImperialInput.classList.remove(".hide");
+  }
+}
+
+inputCheck();
