17e6b723b534c0a44ded0d5b1525660a5dfccd0c
Hemanathan H P <hphemanathan@gmail.com>
2024-09-25 01:19:50 +0530

    wiplp

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const metricButton = document.getElementById('Metric');
 const ImperialButton = document.getElementById("Imperial");
+const MetricInput = document.querySelector(".section__calc-form--metric");
 const ImperialInput = document.querySelector(".section__calc-form--imperial")
 
-console.log (ImperialButton)
+console.log (ImperialButton);
 
-function inputCheck () {
-  if (ImperialButton.checked) {
-    console.log("working");
-    ImperialInput.classList.remove(".hide");
-  }
+function imperialCheck () {
+  MetricInput.classList.add('hide')
+  ImperialInput.classList.remove('hide');
 }
 
-inputCheck();
+function MetricCheck() {
+  MetricInput.classList.remove("hide");
+  ImperialInput.classList.add("hide");
+}
+
+ImperialButton.addEventListener("click", imperialCheck);
+
+metricButton.addEventListener("click", MetricCheck);
+
+// function inputCheck () {
+//   if (ImperialButton.checked) {
+//     console.log("working");
+//     ImperialInput.classList.remove(".hide");
+//   }
+// }
+
+// inputCheck();
